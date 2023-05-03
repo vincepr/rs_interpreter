b2aac88d252c544ba440bbc6648e91868d4626ab
vincepr <vinceprinceking@gmail.com>
2023-05-03 14:56:06 +0200

    Lexer complete added support for - numbers - literals - keywords

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use std::{env, process, fs};
+use std::{env, fs, process};
 
 fn main() {
     println!("Running the LOX interpreter:");
     let args: Vec<String> = env::args().collect();
-    if args.len() == 1{
+    if args.len() == 1 {
         // running as "sanbox-mode"
         rs_interpreter::run_prompt();
-    } else if args.len() == 2{
+    } else if args.len() == 2 {
         // running a file from path
-        if let Some(source_code) = open_file(&args[1]){
+        if let Some(source_code) = open_file(&args[1]) {
             rs_interpreter::run_file(source_code);
         } else {
             process::exit(1);
@@ -18,7 +18,6 @@ fn main() {
     }
 }
 
-fn open_file(path: &str)-> Option<String> {
+fn open_file(path: &str) -> Option<String> {
     fs::read_to_string(path).ok()
-
 }
